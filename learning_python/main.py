f3d254dfc3ee7e00123f4c4953307ba6a4eaf2b9
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2015-08-04 12:39:10 -0400

    Update to learning_python
    Conditionals and Collections

--- learning_python/main.py
+++ learning_python/main.py
@@ -24,3 +24,23 @@ age = current_year - birth_year
 
 print "You are " + str(age) + " years old"
 
+budget = 10000
+
+if budget > 100000:
+    high_car = "A8"
+    print "Yeah, I can afford an " + high_car
+elif budget > 5000:
+    print "Yay! You can afford a nice used car!"
+else:
+    # print "Don't get upset, but you may have to wait a while before buying a car."
+    pass
+
+characters = ["a","b","c"]
+#print characters
+characters.append("d")
+#print characters
+#print characters[2]
+
+alphabet = dict() #initiate dictionary
+alphabet = {"first": "a", "second": "b"}
+print alphabet["first"]
