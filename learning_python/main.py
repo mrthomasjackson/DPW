840344ffbb9039b65423284beb93e7a5bfb53303
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2015-08-04 13:17:00 -0400

    updated learning_python
    Functions and Loops

--- learning_python/main.py
+++ learning_python/main.py
@@ -44,3 +44,55 @@ characters.append("d")
 alphabet = dict() #initiate dictionary
 alphabet = {"first": "a", "second": "b"}
 print alphabet["first"]
+
+#while loop
+'''
+i = 100
+while i>0:
+    print str(i) + " bottles on the wall"
+    i -= 1
+
+#for loop
+
+for i in range(0, 100):
+    print str(i), " bottles on the wall"
+    i = i+1
+
+#for each loop
+family = ["Scott", "Jenny", "Thomas", "Ellen"]
+for f in family:
+    print f + " Jackson"
+
+
+#functions
+
+def convert_cm_in():
+    request = int(raw_input("How many centimeters would you like to convert?"))
+    inches = (request * 2.54)
+    return inches
+
+conversion = convert_cm_in()
+print conversion
+'''
+#inputting variables into larger strings
+
+title = "This is very useful."
+body = "Yes, I agree with you."
+
+html = '''
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>
+            {title}
+        </title>
+    </head>
+    <body>
+        {body}
+    </body>
+</html>
+'''
+html = html.format(**locals())
+print html
+
+
