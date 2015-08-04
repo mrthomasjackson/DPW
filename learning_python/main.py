2139fa06e14efcee682a990dc83738448fd04aa7
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2015-08-04 12:26:54 -0400

    Update to learning_python
    Comments , Variables and Printing in Python

--- learning_python/main.py
+++ learning_python/main.py
@@ -1,3 +1,26 @@
+#I am trying python for the first time
 __author__ = 'tjackson'
 
-print "hello world!"
+welcome_message = "Welcome!"
+space = " "
+
+#this is a one line comment
+'''
+Doc string (multiple line comments)
+'''
+
+first_name = "Thomas"
+last_name = "Jackson"
+#print(first_name + " " + last_name)
+
+#response = raw_input("Enter Your Name")
+
+#print welcome_message + space + response
+
+birth_year = 1993
+current_year = 2015
+age = current_year - birth_year
+#print age
+
+print "You are " + str(age) + " years old"
+
