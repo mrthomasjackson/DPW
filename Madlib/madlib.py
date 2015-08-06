69624c2ea7db23815e9f552b990606fdeb3ab8d1
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2015-08-06 09:36:49 -0400

    Decided On MadLib Topic
    Decided to use the Star Wars opening paragraph as a mad-lib. Created
    the dictionary, array, and questions to input into the two.

--- Madlib/madlib.py
+++ Madlib/madlib.py
@@ -1,3 +1,52 @@
 __author__ = 'tjackson'
 
+# Initiate Dictionary
+dictionary = dict()
+
+dictionary = {"nouns": [], "verbs": []}
+number_questions = []
+
+dictionary["nouns"].append(raw_input("Name a place: "))
+dictionary["nouns"].append(raw_input("Name a type of car: "))
+dictionary["nouns"].append(raw_input("Who is your favorite super-villain? "))
+dictionary["nouns"].append(raw_input("What is a popular children's toy? "))
+dictionary["nouns"].append(raw_input("What type of toy did you just name? "))
+dictionary["nouns"].append(raw_input("Name something really BIG: "))
+dictionary["nouns"].append(raw_input("Let's hear a girls name: "))
+dictionary["verbs"].append(raw_input("What is your favorite physical activity? "))
+dictionary["verbs"].append(raw_input("Say something crazy! "))
+
+number_questions.append(raw_input("How many countries have you traveled to? "))
+number_questions.append(raw_input("What year were you born? "))
+number_questions.append(raw_input("What is your favorite number? "))
+
+
+
+
+'''
+A long time ago, in a galaxy far,
+far away....
+
+It is a period of civil war.
+Rebel spaceships, striking
+from a hidden base, have won
+their first victory against
+the evil Galactic Empire.
+
+During the battle, rebel
+spies managed to steal secret
+plans to the Empire's
+ultimate weapon, the DEATH
+STAR, an armored space
+station with enough power to
+destroy an entire planet.
+
+Pursued by the Empire's
+sinister agents, Princess
+Leia races home aboard her
+starship, custodian of the
+stolen plans that can save
+her people and restore
+freedom to the galaxy....
+'''
 
