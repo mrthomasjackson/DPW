c88f9411b5e43a9869499bea297084c2c35b296d
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2015-08-11 18:43:46 -0400

    finished data objects section

--- data-objects/main.py
+++ data-objects/main.py
@@ -1,24 +1,30 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# data objects
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+
+        luke = Character()
+        luke.name = "Luke Skywalker"
+        luke.profession = "Jedi"
+        luke.age = "26"
+        luke.home_planet = "Tattooine"
+
+        leia = Character()
+        leia.name = "Princess Leia"
+        leia.profession = "Princess"
+        leia.age = luke.age
+        leia.home_planet = "Alderan"
+
+        chars = [luke, leia]
+        print chars
+
+class Character(object):
+    def __init__(self):
+        self.name = ""
+        self.profession = ""
+        self.age = 0
+        self.home_planet = ""
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
