233451cae0f58ef216e174bf3f1e74afcfdb26eb
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2015-08-12 21:42:39 -0400

    Set Up to print HTML to webpage
    page = HTML doctor
     self.response.write(page)

--- simple-form/main.py
+++ simple-form/main.py
@@ -1,24 +1,27 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+'''
+Thomas Jackson
+08/12/2015
+DPW
+Simple Form
+'''
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        page = '''<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Simple Form</title>
+    </head>
+    <body>
+        <form method="GET">
+            <label for="user"></label><input type="text" name="user" />
+            <label for="email"></label><input type="text" name="email" />
+        </form>
+    </body>
+</html>
+        '''
+        self.response.write(page)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
