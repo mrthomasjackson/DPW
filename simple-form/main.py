470cacfbe28db69834caf55e85ebe84202d05cf5
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2015-08-13 00:01:31 -0400

    Revert fddac9e..6cd8c92
    This rolls back to commit fddac9e4546fca073d1c0b34008ffeb577c34092.

--- simple-form/main.py
+++ simple-form/main.py
@@ -7,30 +7,40 @@ Simple Form
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
-    def print_html(self):
-        if self.request.POST:
-            user = self.request.POST['user']
-            email = self.request.POST['email']
-            self.response.write(View.page_head + user + email + self.page_body + self.page_close)
-        else:
-            self.response.write(self.page_head + self.page_body + self.page_close)
+    def get(self):
+        page = View()
+        return page
+    
+
+        self.response.write(page.print_html())
 
 class View(object):
-    page_head = '''<!DOCTYPE HTML>
+    def __init__(self):
+        self.page_head = '''<!DOCTYPE HTML>
 <html>
     <head>
         <title>Simple Form</title>
     </head>
     <body>'''
-    page_body = '''<form method="GET" action="">
+        self.page_body = '''<form method="GET" action="">
             <label for="user">User: </label><input type="text" name="user" />
             <label for="email">Email: </label><input type="text" name="email" />
             <input type="submit" value="submit" />
             </form>'''
-    page_close = '''
+
+        self.page_close = '''
     </body>
 </html>
         '''
+    def print_html(self):
+        return self.page_head + self.page_body + self.page_close
+        # if self.request.GET:
+        #     user = self.request.GET['user']
+        #     email = self.request.GET['email']
+        #     return self.page_head + user + email + self.page_body + self.page_close
+        # else:
+        #     return self.page_head + self.page_body + self.page_close
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
