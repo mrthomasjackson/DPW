14b32657baf746d0f255014e97784ac89d73d901
Thomas Jackson <mrthomasjackson@fullsail.edu>
2015-08-25 21:31:23 -0400

    added mainpage class
    also added js and css directories to app.yaml

--- dynamic-site/main.py
+++ dynamic-site/main.py
@@ -1,8 +1,10 @@
 import webapp2
+from pages import Page
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        p = Page()
+        self.response.write(p.print_out())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
--- dynamic-site/pages.py
+++ dynamic-site/pages.py
@@ -16,3 +16,29 @@ class Page(object):
 
     def print_out(self):
         return self._open + self._body + self._close
+
+class MainPage(Page):
+    def __init__(self):
+        super(MainPage, self).__init__()
+        self._body = '''
+        <form
+        '''
+
+class DistancePage(Page):
+    def __init__(self):
+        super(DistancePage, self).__init__()
+        self._open_form = '<form method="GET">'
+        self._close_form = '</form>'
+        self.__form_arr = []
+        self._form_inputs = ''
+
+    @property
+    def form_arr(self):
+        pass
+
+    @form_arr.setter
+    def form_arr(self, arr):
+        pass
+
+
+
