a610c8507239220e9ed9d3519a46fe0e6de06025
Thomas Jackson <mrthomasjackson@fullsail.edu>
2015-08-26 11:29:01 -0400

    data finalized in subclasses

--- dynamic-site/data.py
+++ dynamic-site/data.py
@@ -60,41 +60,41 @@ class China(Data):
 class India(Data):
     def __init__(self):
         super(India, self).__init__()
-        self.country = 'China'
-        self.capital = 'Beijing'
-        self.currency = 'Renminbi'
-        self.gdp = '9.24 Trillion USD'
-        self.population = '1.34 Billion'
+        self.country = 'India'
+        self.capital = 'New Delhi'
+        self.currency = 'Rupee'
+        self.gdp = '1.88 Trillion USD'
+        self.population = '1.25 Billion'
 
 
 class UnitedStates(Data):
     def __init__(self):
         super(UnitedStates, self).__init__()
-        self.country = 'China'
-        self.capital = 'Beijing'
-        self.currency = 'Renminbi'
-        self.gdp = '9.24 Trillion USD'
-        self.population = '1.34 Billion'
+        self.country = 'United States'
+        self.capital = 'Washington DC'
+        self.currency = 'Dollar'
+        self.gdp = '16.77 Trillion USD'
+        self.population = '318.9 Million'
 
 
 class Indonesia(Data):
     def __init__(self):
         super(Indonesia, self).__init__()
-        self.country = 'China'
-        self.capital = 'Beijing'
-        self.currency = 'Renminbi'
-        self.gdp = '9.24 Trillion USD'
-        self.population = '1.34 Billion'
+        self.country = 'Indonesia'
+        self.capital = 'Jakarta'
+        self.currency = 'Rupiah'
+        self.gdp = '868.3 Billion USD'
+        self.population = '249.9 Million'
 
 
 class Brazil(Data):
     def __init__(self):
         super(Brazil, self).__init__()
-        self.country = 'China'
-        self.capital = 'Beijing'
-        self.currency = 'Renminbi'
-        self.gdp = '9.24 Trillion USD'
-        self.population = '1.34 Billion'
+        self.country = 'Brazil'
+        self.capital = 'Brazilia'
+        self.currency = 'Real'
+        self.gdp = '2.25 Trillion USD'
+        self.population = '200.4 Million'
 
 
 
